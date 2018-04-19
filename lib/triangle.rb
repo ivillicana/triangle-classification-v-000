7db218375dbc70fbf2fb0e6dff47f8a8bd1bd9ff
Isaac Villicana <villicana.i@gmail.com>
2018-04-19 21:33:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,9 @@ class Triangle
   end
 
   def isosceles?
-    if (@side1 == @side2 && @side2 != @side3)
+    if (@side1 == @side2 && @side2 != @side3) ||
+      (@side1 == @side3 && @side3 != @side2) ||
+      (@side2 == @side3 && @side3 != @side1)
       return :isosceles
     end
   end
